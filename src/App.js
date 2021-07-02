ed455959a570ab5eba57b979a6c4141cc0f416e9
basicjohn <edmondsonj@gmail.com>
2021-07-02 12:08:08 -0700

    WIP: app.js creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React, { Component } from 'react';
+// Components
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    let currentlyVisibleState = null;
+
+    if (this.state.currentPage === 'keg') {
+      currentlyVisibleState = <Keg />
+    } else if (this.state.currentPage === 'allKegs') {
+      currentlyVisibleState = <AllKegs />
+    }  else if (this.state.currentPage === 'admin') {
+      // normally check for admin privileges
+      currentlyVisibleState = <Admin createKeg={this.createKeg} />
+    } else {
+      currentlyVisibleState = <Home navigateTo={this.navigateTo} />
+    }
+
+    return (
+      <div className="App">
+        <Header navigateTo={this.navigateTo} />
+        {currentlyVisibleState}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
