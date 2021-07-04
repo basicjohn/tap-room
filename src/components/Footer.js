a15c17bdd96b31ce7c05a32a75297cedbfadeb22
basicjohn <edmondsonj@gmail.com>
2021-07-04 12:40:12 -0700

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,15 @@ function Footer() {
   }
 
   return (
-    <div style={footerStyles}>
-      <h4></h4>
-    </div>
+    <footer style={footerStyles}>
+      <p>
+        This site is not a real brewery and this website was created to display React fundamentals & to display my coding progress on the road to
+        becoming a Full Stack Developer.
+      </p>
+      <a target="_blank" href="https://github.com/basicjohn"
+      >Created by John Edmondson</a
+      >
+    </footer>
   )
 }
 
