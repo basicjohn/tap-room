cee8a3d4694f105caf5140f807ffaa3e3a5c5b44
basicjohn <edmondsonj@gmail.com>
2021-07-04 14:49:10 -0700

    build basic Home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,1 +1,24 @@
 import React from 'react';
+
+function Home(props) {
+
+  return (
+    <main>
+      <div>
+        <a type="submit"
+          onClick={event => props.navigateTo(event, "yearRound")} href='/yearRound'>Beers Available Year Round</a>
+
+        <a type="submit"
+          onClick={event => props.navigateTo(event, "seasonal")} href='/seasonal'>Seasonal Beer</a>
+
+        <a type="submit"
+          onClick={event => props.navigateTo(event, "smallBatch")} href='/smallBatch'>Small Batch Beers</a>
+
+        <a type="submit"
+          onClick={event => props.navigateTo(event, "reserve")} href='/reserve'>Our Select Reserve</a>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
